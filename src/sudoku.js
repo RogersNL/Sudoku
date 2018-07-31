100a3ba2d7e8364400675f16b8fdd7a579380cb8
Colin Bates, Nick Rogers and Swati Sahay <nick@>
2018-07-31 15:50:25 -0700

    Tests pass.

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,4 @@
-function Sudoku(grid){
+export function Sudoku(grid){
   this.grid = grid;
 }
 var compareArray = [1,2,3,4,5,6,7,8,9];
@@ -14,7 +14,7 @@ Sudoku.prototype.rowCzech = function(){
     }
   }
   return true;
-}
+};
 Sudoku.prototype.columnCzech = function(){
   var columns = [];
   for(var i = 0; i < this.grid.length; i++){
@@ -22,88 +22,91 @@ Sudoku.prototype.columnCzech = function(){
       columns.push(this.grid[j][i]);
     }
   }
-  for(var k = 0; k < checkArray.length; k++){
-    var row = checkArray[k].sort();
+  for(var k = 0; k < columns.length; k++){
+    var row = columns[k].sort();
     for(var f = 0; f < row.length; f++){
-      if(row[f] != compareArray[f]){
+      if(row[f] != columns[f]){
         return false;
       }
     }
   }
   return true;
-}
+};
 Sudoku.prototype.cellCzech = function(){
   var cells = [];
+  var row = [];
+  var row1 = [];
+  var row2 = [];
+  var row3 = [];
+  var row4 = [];
+  var row5 = [];
+  var row6 = [];
+  var row7 = [];
+  var row8 = [];
+
   for(var a = 0; a < 3; a++){
     for(var b = 0; b < 3; b++){
-      var row = [];
       row.push(this.grid[a][b]);
-      cells.push(row);
     }
   }
+  cells.push(row);
   for(var c = 0; c < 3; c++){
     for(var d = 3; d < 6; d++){
-      var row = [];
-      row.push(this.grid[c][d]);
-      cells.push(row);
+      row1.push(this.grid[c][d]);
     }
   }
-  for(var e = 0; e < 3; e++)
+  cells.push(row1);
+  for(var e = 0; e < 3; e++){
     for(var f = 6; f < 9; f++){
-      var row = [];
-      row.push(this.grid[e][f]);
-      cells.push(row);
+      row2.push(this.grid[e][f]);
     }
   }
-  for(var g = 3; g < 6; g++)
+  cells.push(row2);
+  for(var g = 3; g < 6; g++){
     for(var h = 0; h < 3; h++){
-      var row = [];
-      row.push(this.grid[g][h]);
-      cells.push(row);
+      row3.push(this.grid[g][h]);
     }
   }
-  for(var i = 3; i < 6; i++)
+  cells.push(row3);
+  for(var i = 3; i < 6; i++){
     for(var j = 3; j < 6; j++){
-      var row = [];
-      row.push(this.grid[i][j]);
-      cells.push(row);
+      row4.push(this.grid[i][j]);
     }
   }
-  for(var k = 3; k < 6; k++)
+  cells.push(row4);
+  for(var k = 3; k < 6; k++){
     for(var l = 6; l < 9; l++){
-      var row = [];
-      row.push(this.grid[k][l]);
-      cells.push(row);
+      row5.push(this.grid[k][l]);
     }
   }
-  for(var m = 6; m < 9; m++)
+  cells.push(row5);
+  for(var m = 6; m < 9; m++){
     for(var n = 0; n < 3; n++){
-      var row = [];
-      row.push(this.grid[m][n]);
-      cells.push(row);
+      row6.push(this.grid[m][n]);
     }
   }
-  for(var o = 6; o < 9; o++)
+  cells.push(row6);
+  for(var o = 6; o < 9; o++){
     for(var p = 3; p < 6; p++){
-      var row = [];
-      row.push(this.grid[g][h]);
-      cells.push(row);
+      row7.push(this.grid[o][p]);
     }
   }
-  for(var q = 6; q < 9; q++)
+  cells.push(row7);
+  for(var q = 6; q < 9; q++){
     for(var r = 6; r < 9; r++){
-      var row = [];
-      row.push(this.grid[q][r]);
-      cells.push(row);
+      row8.push(this.grid[q][r]);
     }
   }
-  for(var s = 0; s < checkArray.length; s++){
-    var row = checkArray[s].sort();
-    for(var t = 0; t < row.length; t++){
-      if(row[t] != compareArray[t]){
+  cells.push(row8);
+  /////////////////////////
+  console.log(cells);
+  for(var s = 0; s < cells.length; s++){
+    var row9 = cells[s].sort();
+    for(var t = 0; t < row9.length; t++){
+      if(row9[t] != compareArray[t]){
         return false;
       }
     }
   }
   return true;
-}
+};
