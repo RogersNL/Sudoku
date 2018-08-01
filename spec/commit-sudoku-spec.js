c7366c6f6f58ca5182a6ae88352533d06a847133
Nick Rogers and Swati Sahay <nick@>
2018-08-01 11:22:18 -0700

    Test rows and columns for same number pass.

--- spec/commit-sudoku-spec.js
+++ spec/commit-sudoku-spec.js
@@ -3,7 +3,7 @@ import { Sudoku } from '../src/sudoku';
 describe('Sudoku', function() {
 
   it('will check for row sudoku', function() {
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -14,11 +14,11 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.rowCzech()).toEqual(true);
   });
   it('will check for column sudoku', function() {
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -29,11 +29,11 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
-    expect(newSudoku.rowCzech()).toEqual(true);
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.columnCzech()).toEqual(true);
   });
   it('will check for cell sudoku', function() {
-    var grid = [
+    let grid = [
       [8,2,4,9,5,3,6,7,1],
       [6,3,5,8,1,7,9,2,4],
       [7,1,9,6,2,4,8,5,3],
@@ -44,7 +44,25 @@ describe('Sudoku', function() {
       [4,7,8,3,6,2,1,9,5],
       [9,5,3,1,7,8,4,6,2]
     ];
-    var newSudoku = new Sudoku(grid);
+    let newSudoku = new Sudoku(grid);
     expect(newSudoku.cellCzech()).toEqual(true);
+  });
+  it('will check for same number in row', function(){
+    let grid = [
+      [8,2,4,9,5,3,6,7,1],
+      [6,3,5,8,1,7,9,2,4],
+      [7,1,9,6,2,4,8,5,3],
+      [5,8,7,2,9,1,3,4,6],
+      [1,4,2,7,3,6,5,8,9],
+      [3,9,6,"",8,5,2,1,7],
+      [2,6,1,5,4,9,7,3,8],
+      [4,7,8,3,6,2,1,9,5],
+      [9,5,3,1,7,8,4,6,2]
+    ];
+    let testNo = 4;
+    let x = 5;
+    let y = 3;
+    let newSudoku = new Sudoku(grid);
+    expect(newSudoku.testRowAndColumn(testNo,x,y)).toEqual(true);
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,144 +1,94 @@
-export function Sudoku(grid){
-  this.grid = grid;
-}
-var compareArray = [1,2,3,4,5,6,7,8,9];
+export class Sudoku {
+  constructor(grid){
+    this.grid = grid;
+  }
 
-Sudoku.prototype.rowCzech = function(){
-  var checkArray = this.grid.slice();
-  for(var i = 0; i < checkArray.length; i++){
-    var row = checkArray[i].sort();
-    for(var j = 0; j < row.length; j++){
-      if(row[j] != compareArray[j]){
-        return false;
+  rowCzech(){
+    const compareArray = [1,2,3,4,5,6,7,8,9];
+
+    let checkArray = this.grid.slice();
+    for(let i = 0; i < checkArray.length; i++){
+      let row = checkArray[i].sort();
+      for(let j = 0; j < row.length; j++){
+        if(row[j] != compareArray[j]){
+          return false;
+        }
       }
     }
+    return true;
   }
-  return true;
-};
-Sudoku.prototype.columnCzech = function(){
-  var columns = [];
-  for(var i = 0; i < this.grid.length; i++){
-    for(var j = 0; j < this.grid.length; j++){
-      columns.push(this.grid[j][i]);
+  columnCzech(){
+    const compareArray = [1,2,3,4,5,6,7,8,9];
+    let checkArray = this.grid.slice();
+    let columns = [];
+    for(let i = 0; i < checkArray.length; i++){
+      let tempRow = [];
+      for(let j = 0; j < checkArray[i].length; j++){
+        tempRow.push(checkArray[j][i]);
+      }
+      columns.push(tempRow);
     }
-  }
-  for(var k = 0; k < columns.length; k++){
-    var row = columns[k].sort();
-    for(var f = 0; f < row.length; f++){
-      if(row[f] != columns[f]){
-        return false;
+    for(let k = 0; k < columns.length; k++){
+      let row = columns[k].sort();
+      for(let f = 0; f < row.length; f++){
+        if(row[f] != compareArray[f]){
+          return false;
+        }
       }
     }
+    return true;
   }
-  return true;
-};
 
-Sudoku.prototype.cellCzech = function(){
- var flag;
-  for(var k=0;k<3;k++){
-    for(var m=0;m<3;m++){
-    flag=this.checkGrid(k*3,m*3);
-   if(flag===false) return false;
- }
-}
-  return true;
-};
-
-Sudoku.prototype.checkGrid = function(i,j){
-    var cells = [];
-    for(var k=0;k<3;k++){
-      for(var m=0;m<3;m++){
-        var a = i+k;
-        var b = j+m;
-        cells.push(this.grid[a][b]);
+  cellCzech(){
+    let flag;
+    for(let k=0;k<3;k++){
+      for(let m=0;m<3;m++){
+        flag=this.checkGrid(k*3,m*3);
+        if(flag===false) return false;
       }
     }
-    var row9 = cells.sort();
-    for(var t = 0; t < row9.length; t++){
-      if(row9[t] != compareArray[t]){
-        return false;
+    return true;
+  }
+
+  checkGrid(i,j){
+    const compareArray = [1,2,3,4,5,6,7,8,9];
+    let checkArray = this.grid.slice();
+    let cells = [];
+    for(let k=0;k<3;k++){
+      for(let m=0;m<3;m++){
+        let a = i+k;
+        let b = j+m;
+        cells.push(checkArray[a][b]);
       }
-
-}
-return true;
-}
-/*
-Sudoku.prototype.cellCzech = function(){
-  var cells = [];
-  var row = [];
-  var row1 = [];
-  var row2 = [];
-  var row3 = [];
-  var row4 = [];
-  var row5 = [];
-  var row6 = [];
-  var row7 = [];
-  var row8 = [];
-
-  for(var a = 0; a < 3; a++){
-    for(var b = 0; b < 3; b++){
-      row.push(this.grid[a][b]);
     }
-  }
-  cells.push(row);
-  for(var c = 0; c < 3; c++){
-    for(var d = 3; d < 6; d++){
-      row1.push(this.grid[c][d]);
-    }
-  }
-  cells.push(row1);
-  for(var e = 0; e < 3; e++){
-    for(var f = 6; f < 9; f++){
-      row2.push(this.grid[e][f]);
-    }
-  }
-  cells.push(row2);
-  for(var g = 3; g < 6; g++){
-    for(var h = 0; h < 3; h++){
-      row3.push(this.grid[g][h]);
-    }
-  }
-  cells.push(row3);
-  for(var i = 3; i < 6; i++){
-    for(var j = 3; j < 6; j++){
-      row4.push(this.grid[i][j]);
-    }
-  }
-  cells.push(row4);
-  for(var k = 3; k < 6; k++){
-    for(var l = 6; l < 9; l++){
-      row5.push(this.grid[k][l]);
-    }
-  }
-  cells.push(row5);
-  for(var m = 6; m < 9; m++){
-    for(var n = 0; n < 3; n++){
-      row6.push(this.grid[m][n]);
-    }
-  }
-  cells.push(row6);
-  for(var o = 6; o < 9; o++){
-    for(var p = 3; p < 6; p++){
-      row7.push(this.grid[o][p]);
-    }
-  }
-  cells.push(row7);
-  for(var q = 6; q < 9; q++){
-    for(var r = 6; r < 9; r++){
-      row8.push(this.grid[q][r]);
-    }
-  }
-  cells.push(row8);
-  /////////////////////////
-  console.log(cells);
-  for(var s = 0; s < cells.length; s++){
-    var row9 = cells[s].sort();
-    for(var t = 0; t < row9.length; t++){
+    let row9 = cells.sort();
+    for(let t = 0; t < row9.length; t++){
       if(row9[t] != compareArray[t]){
         return false;
       }
     }
+    return true;
   }
-  return true;
-};
-*/
+
+  testRowAndColumn(testNo, rowNo, columnNo){
+    for(let j = 0; j < this.grid[rowNo].length; j++){
+      if(this.grid[rowNo][j] === testNo){
+        return false;
+      }
+    }
+    for(let i = 0; i < this.grid.length; i++){
+      if(this.grid[i][columnNo] === testNo){
+        return false;
+      }
+    }
+    return true;
+  }
+  // testSubGrid(testNo, rowNo, columnNo){
+  //
+  // }
+  // isSafe(testNo,i,j){
+  //   let checkArray = this.grid.slice();
+  //   let row = checkArray[i];
+  //
+  // }
+}
